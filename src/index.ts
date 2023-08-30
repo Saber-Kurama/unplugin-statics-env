4ce5068c171735a695a77d30576d0b26dd2d1a02
guochao <463561248@qq.com>
2023-08-30 17:54:22 +0800

    ✨ feat:  发布0.1.0版本

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,77 @@
+import fsp from 'node:fs/promises'
+import fs from 'node:fs'
+import path from 'node:path'
 import type { UnpluginFactory } from 'unplugin'
 import { createUnplugin } from 'unplugin'
 import type { Options } from './types'
+import { UserConfig, mergeAlias } from 'vite'
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-starter',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
-})
+
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (options = { assert: true, public: true, mode: '' }) => {
+  if (options.assert !== false ) { options.assert = true}
+  if (options.public !== false) { options.public = true }
+  let confgPublicDir: string;
+  let confgAssertsDir: string;
+  if (options.mode) { 
+    const rootPath = process.cwd(); // 还有别方案获取 pkg的根目录吗
+    const cacheDir = path.join(rootPath, 'node_modules/.saber');
+    if (options.public) {
+      const cachePublicDir = path.join(cacheDir, "public");
+      confgPublicDir = cachePublicDir;
+      const publicDir = path.join(rootPath, 'public');
+      const publicModeDir = path.join(rootPath, "statics", options.mode, 'public');
+      const copyPublic = async () => {
+        if (!fs.existsSync(cachePublicDir)) {
+          await fsp.mkdir(cachePublicDir, { recursive: true })
+        }
+        await fsp.cp(publicDir, cachePublicDir, { recursive: true })
+        if (!fs.existsSync(publicModeDir)) {
+          await fsp.cp(publicModeDir, cachePublicDir, { recursive: true })
+        }
+      }
+      copyPublic();
+    }
+    if (options.assert) {
+      const cacheAssetsDir = path.join(cacheDir, 'assets');
+      confgAssertsDir = cacheAssetsDir;
+      const assetsDir = path.join(rootPath, 'src/assets');
+      const assetsModeDir = path.join(rootPath, "statics", options.mode, 'public');
+      const copyAsserts = async () => {
+        if (!fs.existsSync(cacheAssetsDir)) {
+          await fsp.mkdir(cacheAssetsDir, { recursive: true })
+        }
+        await fsp.cp(assetsDir, cacheAssetsDir, { recursive: true })
+        if (!fs.existsSync(assetsModeDir)) {
+          await fsp.cp(assetsModeDir, cacheAssetsDir, { recursive: true })
+        }
+      }
+      copyAsserts();
+    }
+
+  }
+  
+  return {
+    name: 'unplugin-statics-env',
+    async config(config: UserConfig) { 
+      if (options.public) {
+        config.publicDir = confgPublicDir; 
+      }
+      if (options.assert) { 
+        // const a = [
+        //   { find: /.*\/assets/, replacement: path.resolve(process.cwd(), "./node_modules/.saber/assets/") },
+        // ]
+         const a = [
+          { find: /.*\/assets/, replacement: confgAssertsDir },
+        ]
+        const resolvedAlias = mergeAlias(config.resolve?.alias || [], a)
+        if (!config.resolve) { 
+          config.resolve = {};
+        }
+        config.resolve.alias = resolvedAlias
+      }
+    } 
+  }
+}
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 export interface Options {
   // define your plugin options here
+  assert?: boolean;
+  public?: boolean;
+  mode?: string
 }
